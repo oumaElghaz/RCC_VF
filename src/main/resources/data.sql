e500791dd71f5a106d4016b0e03d6913cec46ac1
Oumaima <elghazouaniouma12@gmail.com>
2026-02-11 04:18:12 +0100

    partie de la generation du fichier xml de client statique_ajout_insert_pp_data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -44,8 +44,29 @@ VALUES ('2026-02-18 10:06:24.000000', '2026-02-03 10:06:24.000000', '12', '', ''
 INSERT INTO  sss_cdr_mapping  ( dmo ,  dou ,  id ,  att1 ,  att2 ,  att3 ,  codCibl ,  codSrc ,  ctab ,  descEvo ,  dom ,  libelle ,  uti )
 VALUES ('2026-02-18 10:06:24.000000', '2026-02-03 10:06:24.000000', '13', '', '', '', '01', 'N1', 'QualAcadem', '', '', '', 'GGD');
 
+--sexe M
+INSERT INTO  sss_cdr_mapping  ( dmo ,  dou ,  id ,  att1 ,  att2 ,  att3 ,  codCibl ,  codSrc ,  ctab ,  descEvo ,  dom ,  libelle ,  uti )
+VALUES ('2026-02-18 10:06:24.000000', '2026-02-03 10:06:24.000000', '14', '', '', '', 'M', '1', 'SEXE', '', '', '', 'GGD');
 
---client snapshot 1
+--sitfamil  SIT_F
+INSERT INTO  sss_cdr_mapping  ( dmo ,  dou ,  id ,  att1 ,  att2 ,  att3 ,  codCibl ,  codSrc ,  ctab ,  descEvo ,  dom ,  libelle ,  uti )
+VALUES ('2026-02-18 10:06:24.000000', '2026-02-03 10:06:24.000000', '15', '', '', '', 'C', 'C', 'SIT_F', '', '', '', 'GGD');
+
+--codcat RSDT
+INSERT INTO  sss_cdr_mapping  ( dmo ,  dou ,  id ,  att1 ,  att2 ,  att3 ,  codCibl ,  codSrc ,  ctab ,  descEvo ,  dom ,  libelle ,  uti )
+VALUES ('2026-02-18 10:06:24.000000', '2026-02-03 10:06:24.000000', '16', '', '', '', '1', 'MAR', 'RSDT', '', '', '', 'GGD');
+
+
+--PROF cat profesionnel
+INSERT INTO  sss_cdr_mapping  ( dmo ,  dou ,  id ,  att1 ,  att2 ,  att3 ,  codCibl ,  codSrc ,  ctab ,  descEvo ,  dom ,  libelle ,  uti )
+VALUES ('2026-02-18 10:06:24.000000', '2026-02-03 10:06:24.000000', '17', '', '', '', '5', '0', 'PROF', '', '', '', 'GGD');
+
+
+
+
+
+
+--client snapshot 1 PM
 INSERT INTO SSS_CDR_SNAPSHOT_CLIENT_STAT (altCodClient, entLieeEtab, dateDeclaration, dtRefent, codAgEcon, codClient, entDeclar, entObserv, id_client, natClient, actionType)
 VALUES ('1', 'EAG', '2026-02-18 10:06:24.000000', '2026-02-18 10:06:24.000000', '100', '01212', 'bdhhdnhd', 'nbdbdbdbdb', '01212', 'P', 'ED');
 
@@ -64,7 +85,7 @@ INSERT INTO SSS_CDR_SNAPSHOT_CLIENT_BENEF (ID, IDBENEFFECT, ID_CLIENT, NATBENEFF
 VALUES ('1', '15655', '01212', 'P', 'hjdshjdshj', 'hgszgs', 'CIN');
 
 
---client snapshot 2
+--client snapshot 2 PM
 INSERT INTO SSS_CDR_SNAPSHOT_CLIENT_STAT (altCodClient, entLieeEtab, dateDeclaration, dtRefent, codAgEcon, codClient, entDeclar, entObserv, id_client, natClient, actionType)
 VALUES ('2', 'EAG', '2025-02-18 10:06:24.000000', '2025-02-18 10:06:24.000000', '100', '01215', 'bdhhdnhd', 'nbdbdbdbdb', '01215', 'P', 'ED');
 
@@ -81,3 +102,20 @@ VALUES ('11', '2', '1', 'A', 'sss', '01215', '96655', 'ddff', 'ffff', 'P', 'eee'
 --beneficiaire 2
 INSERT INTO SSS_CDR_SNAPSHOT_CLIENT_BENEF (ID, IDBENEFFECT, ID_CLIENT, NATBENEFFECT, NOMBENEFFECT, PREBENEFFECT, TYPIDBENEFFECT)
 VALUES ('2', '15655', '01215', 'P', 'hjdshjdshj', 'hgszgs', 'CIN');
+
+
+--client snapshot 3 PP
+INSERT INTO SSS_CDR_SNAPSHOT_CLIENT_STAT (altCodClient, entLieeEtab, dateDeclaration, dtRefent, codAgEcon, codClient, entDeclar, entObserv, id_client, natClient, actionType)
+VALUES ('3', 'EAG', '2025-02-18 10:06:24.000000', '2025-02-18 10:06:24.000000', '100', '01219', 'bdhhdnhd', 'nbdbdbdbdb', '01219', 'P', 'ED');
+INSERT INTO  donneesintpp  (idPrincipal,tpIdPrincipal,prenom,nomFamille,paysDelivrance,dtDelivrance,dtExpiration,TypePPPro,RNAE,dtNaissance,codLocalNaissance,sexe,nationalite,sitFamille,codCatProf,menage,qualAcadem,catClient,  id_client)
+VALUES ('1', 'CIN', 'John', 'Doe', 'AF', '2026-02-18 09:58:07.000000', '2023-02-18 09:58:07.000000', '100', 'RNAEValue', '1995-12-31 23:59:59.000000', 'AZ', '1', 'AFG', 'C', '0', 5, 'N1', 'MAR','01219');
+--adresse 3
+
+INSERT INTO  adresse  ( id ,  adresse ,  codLocal ,  codPays ,  codPostal ,  id_client ,  numTeleph )
+VALUES ('3', 'hhgghggghgh', 'AZ', 'AF', '545', '01219', '555415');
+--actionnaire 3
+INSERT INTO  sss_cdr_snapshot_client_act  ( qtpartCapSocAct ,  id ,  codTribunAct ,  formJurAct ,  iceact ,  id_client ,  idPrincAct ,  idSpecifiqueAct ,  leiact ,  natActionnaire ,  nomRaisonSocAct ,  payResAct ,  regCommerAct ,  tpIdPrincAct )
+VALUES ('11', '3', '1', 'A', 'sss', '01219', '96655', 'ddff', 'ffff', 'P', 'eee', 'AF', 'DDDD', 'CIN');
+--beneficiaire 3
+INSERT INTO SSS_CDR_SNAPSHOT_CLIENT_BENEF (ID, IDBENEFFECT, ID_CLIENT, NATBENEFFECT, NOMBENEFFECT, PREBENEFFECT, TYPIDBENEFFECT)
+VALUES ('3', '15655', '01219', 'P', 'hjdshjdshj', 'hgszgs', 'CIN');
